7f9ffe92cc3a73509099f4c18a2bea0d3ed5ecc8
Your Name <you@example.com>
2023-01-18 18:40:11 +0200

    commi

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request 
 import pytest
-
+# parser = argparse.ArgumentParser()
+# parser.add_argument("--trustkey_jks", action="store_true")
 app = Flask(__name__)
 
 @app.route('/api', methods=['POST'])
@@ -11,3 +12,5 @@ def check_packages():
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 # curl -X POST http://localhost:5000/api -H "Content-Type: application/json" -d '{"packages": ["pkg1", "pkg2", "pkg3", "pkg4", "pkg5", "pkg6", "pkg7", "pkg8", "pkg9", "pkg10"]}'
+# pip install pytest-xdist
+
